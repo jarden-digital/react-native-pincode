6b71c82ef22042ad15295274bbac32a97ad98994
Haskkor <haskkor@gmail.com>
2018-07-23 19:13:18 +1200

    Working on the tests

--- __tests__/PinCodeTest.js
+++ __tests__/PinCodeTest.js
@@ -1,13 +1,37 @@
 // __tests__/PinCodeTest.js
 import 'react-native'
 import React from 'react'
-import PinCode from 'dist/src/PinCode'
+import PinCode from '../dist/src/PinCode'
+import PinCodeChoose from '../dist/src/PinCodeChoose'
+import PinCodeEnter from '../dist/src/PinCodeEnter'
+import ApplicatinLocked from '../dist/src/ApplicationLocked'
 
 import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
+it('PinCode renders correctly', () => {
     const tree = renderer.create(
         <PinCode />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('PinCodeChoose renders correctly', () => {
+    const tree = renderer.create(
+        <PinCodeChoose />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('PinCodeEnter renders correctly', () => {
+    const tree = renderer.create(
+        <PinCodeEnter />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('ApplicatinLocked renders correctly', () => {
+    const tree = renderer.create(
+        <ApplicatinLocked />
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
